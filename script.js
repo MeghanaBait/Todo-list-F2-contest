f6882d394fa0b8d2be2be0c2552215c9c44f60ca
MeghanaBait <meghanawork777@gmail.com>
2024-01-04 22:00:14 +0530

    add 3

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const inputBox = document.getElementById("input-box");
+const inputBox = document.getElementById("task-input");
+const dueDateInput = document.getElementById('due-date-input');
+const priorityInput = document.getElementById('priority-input');
+const statusInput = document.getElementById('status-input');
+const todoTable = document.getElementById('todo-table');
+const todoCountTable = document.getElementById('todoCount');
 const listContainer = document.getElementById("list-container");
 const noTaskDiv = document.querySelector(".no-task");
 
@@ -8,9 +13,25 @@ function addTask(){
     }
     else{
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        let nameDiv = document.createElement("div");
+        nameDiv.classList.add('TaskName');
+        let dateDiv = document.createElement("div");
+        let priorityDiv = document.createElement("div");
+        let statusDiv = document.createElement("div");
+        const editBtn = document.createElement('button');
+        editBtn.textContent = 'Edit';
+        editBtn.classList.add("edit");
         let deleteSpan = document.createElement("span");
-        li.appendChild(deleteSpan);
+
+
+        //content adding
+        nameDiv.textContent= inputBox.value;
+        dateDiv.textContent= dueDateInput.value;
+        priorityDiv.textContent =priorityInput.value;
+        statusDiv.textContent = statusInput.value;
+
+        //adding
+        li.append(nameDiv, dateDiv, priorityDiv, statusDiv, editBtn, deleteSpan);
 
         listContainer.appendChild(li);
 
@@ -18,16 +39,35 @@ function addTask(){
     }
     // clear input 
     inputBox.value="";
+    dueDateInput.value="";
+    priorityInput.value="";
+    statusInput.value="";
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('checked');
+        saveData();
     } else if (e.target.tagName ==="SPAN") {
         e.target.parentElement.remove();
+        saveData();
         if (listContainer.querySelectorAll('li').length === 0) {
             noTaskDiv.style.display = "flex";
         }
     }
 }, false);
+
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
 
